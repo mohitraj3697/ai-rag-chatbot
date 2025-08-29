8bfd6e48f6b08e3e4b043d89cafc11ff5e785438
mohitraj3697 <mohitraj3697@gmail.com>
2025-08-30 02:34:47 +0530

    Update6 script.js

--- frontend/script.js
+++ frontend/script.js
@@ -42,7 +42,7 @@ async function generate(text) {
 }
 
 async function callServer(inputText) {
-  const response = await fetch('https://ai-rag-chatbot-backend.onrender.com', {
+  const response = await fetch('https://ai-rag-chatbot-one.vercel.app', {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({ message: inputText }),
